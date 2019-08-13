99850ef98067ba87016b9e6cc2913768a84c31b2
channu <channu.ec013@gmail.com>
2019-08-13 15:42:58 +0530

    database

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -8,6 +8,7 @@
 namespace App\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 /**
  * Description of ArticleController
@@ -16,15 +17,16 @@ use Symfony\Component\HttpFoundation\Response;
  */
 class ArticleController extends AbstractController {
     /**
-     * @Route("/")
+     * @Route("/" ,name="app_homepage")
      */
     public function homepage(){
         //return new Response('Woo this is my first page');
-        return new Response('This is my first page');
+        //return new Response('This is my first page');
+        return $this->render('articls/homepage.html.twig');
     }
 
     /**
-     * @Route("/news/{newshowpage}")
+     * @Route("/news/{newshowpage}", name="app_showarticle" )
      */
     public function show($newshowpage){
         /*return new Response(sprintf(
@@ -34,10 +36,19 @@ class ArticleController extends AbstractController {
             'Woohoo! I\'m going on an all-asteroid diet!',
             'I like bacon too! Buy some from my site! bakinsomebacon.com',
         ];
+        //dump($newshowpage,$this);
         return $this->render('articls/show.html.twig',[
             'title' => ucwords(str_replace('-','',$newshowpage)),
+            'slug' => $newshowpage,
             'comments' => $comments
         ]);
 
+    }
+
+    /**
+     * @Route("/news/{slug}/heart", name="app_article_heart",methods={"POST"})
+     */
+    public function getheartLikeCounts($slug){
+        return new JsonResponse(['heart'=> rand(5,100)]);
     }
 }
